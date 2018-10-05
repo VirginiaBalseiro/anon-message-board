2091099f3cb5d858ae74dbc519c091d45e7a47e7
Glitch (fcc-anon-message-board-app) <none>
2018-10-05 15:30:49 +0000

    add new reply functionality

---
+++ controllers/ReplyHandler.js
@@ -0,0 +1,47 @@
+var mongo = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var url = process.env.DB;
+
+const ReplyHandler = {
+
+//   getThreads: function(req, res){
+//      var board = req.params.board ? req.params.board : '';
+//      mongo.connect(url, function(err, db) {
+//           var collection = db.collection(board);
+//          collection.find({}).toArray(function(err, docs){
+//            res.json(docs)
+//       });
+        
+//    })
+//      },
+  
+   addReply: function(req, res){
+     console.log(req)
+     var board = req.params.board ? req.params.board : '';
+     var thread_id = req.body.thread_id;
+     console.log(thread_id)
+      var reply = {
+        _id: new ObjectId(),
+         text: req.body.text,
+         created_on: new Date(),
+         reported: false,
+         delete_password: req.body.delete_password
+        }
+    mongo.connect(url, function(err, db) {
+      var collection = db.collection(board);
+      collection.findAndModify(
+        {_id: new ObjectId(thread_id)},
+        [],
+        {
+          $set: {bumped_on: new Date()},
+          $push: {replies: reply},
+        },
+        function(err, doc) {
+          console.log(doc)
+        });
+       res.redirect('/b/'+board+'/'+req.body.thread_id);
+    });   
+    }
+
+ }
+module.exports = ReplyHandler;
